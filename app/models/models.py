b87c06843fd4ffd9bcb3ccea48a6eab2edffdaba
Braulio Alejandro Navarrete Horta <133619100+BraulioAlejandroNavarreteHorta@users.noreply.github.com>
2024-11-25 23:54:47 -0600

    [ADD] file database.py structure

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, Table
 from sqlalchemy.orm import relationship
-from .database import Base
+from ..db.database import Base
 
 # Tabla intermedia para la relación muchos a muchos entre alumnos y materias
 alumno_materia = Table(
